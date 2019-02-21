b97c448df21cdbd30480dd4e276f3a8dcfffb394
Tom Bilbe <tbilbe@gmail.com>
2019-02-21 21:25:58 +0000

    updated grow function to include fitness and hunger functionality

--- __tests__/pet.tests.js
+++ __tests__/pet.tests.js
@@ -11,20 +11,39 @@ it('Should create a new Pet with a name when called', () => {
 });
 
 describe('Growing up!', () => {
-  const pet = new Pet('Logan');
+  let pet
 
-  beforeAll(() => {
-    // Pet should have initial age of 0
-    // initialise the test - beforeEvery and reset the pet to 0
-    expect(pet.name).toBe('Logan');
-    expect(pet.age).toBe(0);
+  beforeEach(() => {
+    pet = new Pet('Logan');
   })
 
-  it('increase age by one', () => {
+  it('grow method should increase age by one', () => {
     pet.grow();
     expect(pet.age).toBe(1);
   })
+})
 
-  // grow up method should increase age by 1;
+describe('Gettting older and Unhealthy!', () => {
+  let pet;
+  beforeEach(() => {
+    pet = new Pet('Logan');
+  })
 
+  it('grow method should increase hunger by 5', () => {
+    expect(pet.hunger).toBe(0);
+    pet.grow();
+    expect(pet.hunger).toBe(5);
+    pet.grow();
+    expect(pet.hunger).toBe(10);
+
+  });
+  it('grow method should decrease fitness by 3', () => {
+
+    expect(pet.fitness).toBe(10);
+    pet.grow();
+    expect(pet.fitness).toBe(7);
+    pet.grow();
+    expect(pet.fitness).toBe(4);
+    pet.grow();
+  });
 })
--- src/pet.js
+++ src/pet.js
@@ -1,10 +1,14 @@
 function Pet(name) {
   this.name = name;
   this.age = 0;
+  this.fitness = 10;
+  this.hunger = 0;
 }
 
 Pet.prototype.grow = function () {
   this.age += 1;
+  this.hunger += 5;
+  this.fitness -= 3;
 }
 
 module.exports = { Pet };
