2a1edccbb8cf75b20105501fecf12af5e138156e
Tom Bilbe <tbilbe@gmail.com>
2019-02-21 20:59:45 +0000

    added grow functionality to Pet.prototype

--- __tests__/pet.tests.js
+++ __tests__/pet.tests.js
@@ -6,7 +6,25 @@ it('returns an objct', () => {
 
 
 it('Should create a new Pet with a name when called', () => {
-  const cat = new Pet('Oscar')
+  const cat = new Pet('Oscar');
   expect(cat.name).toBe('Oscar');
-  // expect(Pet('Fido')).toBe('Fido');
 });
+
+describe('Growing up!', () => {
+  const pet = new Pet('Logan');
+
+  beforeAll(() => {
+    // Pet should have initial age of 0
+    // initialise the test - beforeEvery and reset the pet to 0
+    expect(pet.name).toBe('Logan');
+    expect(pet.age).toBe(0);
+  })
+
+  it('increase age by one', () => {
+    pet.grow();
+    expect(pet.age).toBe(1);
+  })
+
+  // grow up method should increase age by 1;
+
+})
