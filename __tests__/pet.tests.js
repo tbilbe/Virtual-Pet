0e86deb61f0f2c6720ecf6a19b28d57d5c6b2bfb
Tom Bilbe <tbilbe@gmail.com>
2019-02-22 19:03:02 +0000

    keepig fit completed

--- __tests__/pet.tests.js
+++ __tests__/pet.tests.js
@@ -4,41 +4,34 @@ it('returns an objct', () => {
   expect(new Pet('Fido')).toBeInstanceOf(Object);
 })
 
-
 it('Should create a new Pet with a name when called', () => {
   const cat = new Pet('Oscar');
   expect(cat.name).toBe('Oscar');
 });
 
 describe('Growing up!', () => {
-  let pet
-
-  beforeEach(() => {
-    pet = new Pet('Logan');
-  })
-
-  it('grow method should increase age by one', () => {
-    pet.grow();
-    expect(pet.age).toBe(1);
-  })
-})
-
-describe('Gettting older and Unhealthy!', () => {
   let pet;
   beforeEach(() => {
     pet = new Pet('Logan');
   })
-
+  it('grow method should increase age by one', () => {
+    pet.grow();
+    expect(pet.age).toBe(1);
+  })
+});
+describe('Gettting older and Unhealthy!', () => {
+  let pet;
+  beforeEach(() => {
+    pet = new Pet('Logan');
+  });
   it('grow method should increase hunger by 5', () => {
     expect(pet.hunger).toBe(0);
     pet.grow();
     expect(pet.hunger).toBe(5);
     pet.grow();
     expect(pet.hunger).toBe(10);
-
   });
   it('grow method should decrease fitness by 3', () => {
-
     expect(pet.fitness).toBe(10);
     pet.grow();
     expect(pet.fitness).toBe(7);
@@ -46,4 +39,24 @@ describe('Gettting older and Unhealthy!', () => {
     expect(pet.fitness).toBe(4);
     pet.grow();
   });
-})
+});
+describe('Keeping Fit', () => {
+  let pet;
+  beforeEach(() => {
+    pet = new Pet('Logan');
+    pet.grow();
+    pet.grow();
+  });
+  it('should increase the fitness by 4', () => {
+    expect(pet.fitness).toBe(4);
+    pet.walk()
+    expect(pet.fitness).toBe(8)
+  });
+  it('should not exceed fitness property by 10', () => {
+    expect(pet.fitness).toBe(4);
+    pet.walk();
+    pet.walk();
+    pet.walk();
+    expect(pet.fitness).toBe(10);
+  });
+});
--- src/pet.js
+++ src/pet.js
@@ -1,6 +1,7 @@
 function Pet(name) {
   this.name = name;
   this.age = 0;
+  this._fitnessLimiter = 7;
   this.fitness = 10;
   this.hunger = 0;
 }
@@ -9,6 +10,13 @@ Pet.prototype.grow = function () {
   this.age += 1;
   this.hunger += 5;
   this.fitness -= 3;
+}
+
+Pet.prototype.walk = function () {
+  if (this.fitness > this._fitnessLimiter) {
+    return this.fitness = 10;
+  }
+  this.fitness += 4;
 }
 
 module.exports = { Pet };
