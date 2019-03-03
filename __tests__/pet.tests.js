57d9653d01f9a24ab1bbdec62e11a22c977d9c33
Tom Bilbe <tbilbe@gmail.com>
2019-03-03 12:00:12 +0000

    updated Pet constructor functionality to include age, fitness, hunger

--- __tests__/pet.tests.js
+++ __tests__/pet.tests.js
@@ -1,6 +1,41 @@
 const { Pet } = require('../src/pet.js');
 
 describe('create a new instance of a pet', () => {
-  expect(new Pet('Oscar')).toEqual({ name: 'Oscar' });
-  expect(new Pet('Logan')).toEqual({ name: 'Logan' })
+  test('new pet object creation', () => {
+    expect(new Pet('Oscar')).toEqual({ name: 'Oscar', age: 0, hunger: 0, fitness: 10 });
+    expect(new Pet('Logan')).toEqual({ name: 'Logan', age: 0, hunger: 0, fitness: 10 });
+  });
+});
+
+describe('Aging - being born and getting older', () => {
+  let kitty;
+  beforeAll(() => {
+    kitty = new Pet('Oscar');
+  });
+  test('How old are you when you\'re born?', () => {
+    expect(kitty.age).toBe(0);
+  });
+  test('What happens when you grow? - you get older!', () => {
+    kitty.growUp();
+    expect(kitty.age).toBe(1);
+  });
+});
+
+describe('Getting older and unhealthier!', () => {
+  let kitty;
+  beforeAll(() => {
+    kitty = new Pet('Oscar');
+  });
+  test('I am born but I am not hungry...', () => {
+    expect(kitty.hunger).toBe(0);
+    expect(kitty.fitness).toBe(10);
+  });
+  test('We get hungry when we grow', () => {
+    kitty.growUp();
+    expect(kitty.hunger).toBe(5);
+  });
+  test('We get unfit when we grow', () => {
+    kitty.growUp();
+    expect(kitty.fitness).toBe(4);
+  });
 })
--- src/pet.js
+++ src/pet.js
@@ -1,9 +1,19 @@
 function Pet(name) {
   this.name = name;
+  this.age = 0;
+  this.hunger = 0;
+  this.fitness = 10;
 }
 
+Pet.prototype.growUp = function () {
+  const oneYearOlder = 1;
+  const tummyRumbles = 5;
+  const unhealthy = 3;
+  this.age += oneYearOlder;
+  this.hunger += tummyRumbles;
+  this.fitness -= unhealthy;
+}
 
-
-module.export = {
+module.exports = {
   Pet
 }
